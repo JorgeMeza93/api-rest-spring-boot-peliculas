4f1c270fafdb96a841afdb1dd6023657fc20259c
Jorge Meza Bello <jorberiver@gmail.com>
2023-02-16 23:24:08 -0600

    Comienzo del proyecto

---
+++ src/test/java/com/peliculas/peliculas/SpringBootPeliculasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peliculas.peliculas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPeliculasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
