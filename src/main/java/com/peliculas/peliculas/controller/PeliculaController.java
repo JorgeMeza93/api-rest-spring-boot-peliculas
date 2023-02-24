eebfbb3d35465c099a270557e48930d34f3f6481
Jorge Meza Bello <jorberiver@gmail.com>
2023-02-24 00:51:20 -0600

    Obtener Película por id listo

--- src/main/java/com/peliculas/peliculas/controller/PeliculaController.java
+++ src/main/java/com/peliculas/peliculas/controller/PeliculaController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,10 +18,17 @@ import com.peliculas.peliculas.service.PeliculaService;
 public class PeliculaController {
 	
 	@Autowired
-	private IPeliculaService peliculaService;
+	private PeliculaService peliculaService;
 	
 	@GetMapping
 	public ResponseEntity<List<Pelicula>> obtenerPeliculas(){
 		return ResponseEntity.ok(peliculaService.findAll());
 	}
+	@GetMapping("/{id}")
+	public ResponseEntity<Pelicula> obtenerPorId(@PathVariable("id") Integer idPelicula){
+		return peliculaService.findById(idPelicula)
+				.map(ResponseEntity::ok)
+				.orElseGet( () -> ResponseEntity.notFound().build());
+	}
+	
 }
