25426f7ddb62e4f01cf7d3fb6579afce6fda6947
Jorge Meza Bello <jorberiver@gmail.com>
2023-02-24 23:37:31 -0600

    Crear película nueva

--- src/main/java/com/peliculas/peliculas/controller/PeliculaController.java
+++ src/main/java/com/peliculas/peliculas/controller/PeliculaController.java
@@ -3,9 +3,12 @@ package com.peliculas.peliculas.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,6 +32,15 @@ public class PeliculaController {
 		return peliculaService.findById(idPelicula)
 				.map(ResponseEntity::ok)
 				.orElseGet( () -> ResponseEntity.notFound().build());
+	}
+	@GetMapping("/saludo")
+	public ResponseEntity<String> prueba(){
+		String saludo = "Hola Mundo";
+		return new ResponseEntity<>("Hello World", HttpStatus.OK);
+	}
+	@PostMapping
+	public ResponseEntity<Pelicula> crearPelicula(@RequestBody Pelicula pelicula){
+		return new ResponseEntity<>(peliculaService.create(pelicula), HttpStatus.CREATED);
 	}
 	
 }
